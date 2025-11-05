8e673f1ec236b7a510a536a36eddf302fb9addb8
andru-1987 <andru.ocatorres@gmail.com>
2025-11-04 21:02:00 -0300

    Agrego config para GCLOUD

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ferreteria-app/"
+  base: "/ferreteria-app/",
+  server:{
+    port: process.env.PORT || 5173, 
+    host: '0.0.0.0'
+  }
 
 })
