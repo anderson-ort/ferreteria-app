9a946ca1635f49570f38b05bc691544551fdd615
andru-1987 <andru.ocatorres@gmail.com>
2025-11-06 09:35:20 -0300

    add env for path level repo react apply config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.PUBLIC_BASE_PATH || "/",
+  base: process.env.VITE_PUBLIC_BASE_PATH || "/",
   server:{
     port: process.env.PORT || 8080, 
     host: '0.0.0.0'
