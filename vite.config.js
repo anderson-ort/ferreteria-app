ecdbf081c16de1b94301058f9f242f68bee8f9ed
andru-1987 <andru.ocatorres@gmail.com>
2025-11-06 08:28:59 -0300

    add docker compose file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/ferreteria-app/",
   base: "/",
   server:{
     port: process.env.PORT || 8080, 
